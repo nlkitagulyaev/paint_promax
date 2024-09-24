e3e0a421ab59034920b24bfc3e5801b0028a7ba5
nlkitagulyaev <161570202+nlkitagulyaev@users.noreply.github.com>
2024-09-24 18:27:20 +0300

    Add files via upload

---
+++ paint_promax-Branchsave/PaintProMax/Figure.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PaintProMax
+{
+    [Serializable]
+    class Figure
+    {
+        ///////////////////////
+        private int x;
+        private int y;
+        ///////////////////////
+        private int x2;
+        private int y2;
+        ///////////////////////
+        private Color color;
+        public Figure(int X , int Y, int X2, int Y2 , Color Color)
+        {
+            x = X;
+            y = Y;
+            x2 = X2;
+            y2 = Y2;
+            color = Color;
+        }
+        public Figure(int X, int Y, int X2, int Y2)
+        {
+            x = X;
+            y = Y;
+            x2 = X2;
+            y2 = Y2;
+        }
+        public Figure(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int X2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+        public int Y2
+        {
+            get { return y2; }
+            set { y2 = value; }
+        }
+
+
+        public Color Color
+        {
+            get { return color; } 
+            set { color = value; } 
+        }
+
+
+    }
+}
